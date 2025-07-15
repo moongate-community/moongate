175e06cba728bf24912886d1d04a3a49b73d5628
Tom <squid@stormwind.it>
2025-07-15 10:02:55 +0200

    feat(packets): add GetPlayerStatusPacket for handling player status requests
    Introduce GetPlayerStatusPacket class to manage player status
    requests in the game. This addition allows the server to read
    and process player status types and mobile IDs, enhancing
    functionality for character management.
    
    feat(types): add GetPlayerStatusType enum for player status types
    
    Add GetPlayerStatusType enumeration to define various player
    status types such as GodClient, BasicStatus, and RequestSkills.
    This provides a structured way to handle different status
    requests, improving code readability and maintainability.

---
+++ src/Moongate.UO.Data/Packets/Characters/GetPlayerStatusPacket.cs
@@ -0,0 +1,30 @@
+using Moongate.Core.Server.Packets;
+using Moongate.Core.Spans;
+using Moongate.UO.Data.Ids;
+using Moongate.UO.Data.Types;
+
+namespace Moongate.UO.Data.Packets.Characters;
+
+public class GetPlayerStatusPacket : BaseUoPacket
+{
+
+
+    public GetPlayerStatusType StatusType { get; set; }
+    public Serial MobileId { get; set; }
+
+
+    public GetPlayerStatusPacket() : base(0x34)
+    {
+    }
+
+
+    protected override bool Read(SpanReader reader)
+    {
+
+        reader.ReadInt32();
+        StatusType = (GetPlayerStatusType)reader.ReadByte();
+        MobileId = (Serial)reader.ReadUInt32();
+
+        return true;
+    }
+}
