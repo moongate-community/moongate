473b7b3fd3a6f4ad2bd921fdf8f84b71b3305825
tom <squid@stormwind.it>
2026-03-25 13:48:47 +0100

    feat: map resistances and damage types from template to mobile entity (#171)
    - Map MobileTemplateDefinition.Resistances to UOMobileEntity.BaseResistances
      (physical, fire, cold, poison, energy) in CreateMobileFromTemplate()
    - Store MobileTemplateDefinition.DamageTypes as custom string property
      using new MobileCustomParamKeys.Combat.DamageTypes key
    - Add Combat nested class to MobileCustomParamKeys for combat-related keys

--- src/Moongate.Server/Data/Internal/Scripting/MobileCustomParamKeys.cs
+++ src/Moongate.Server/Data/Internal/Scripting/MobileCustomParamKeys.cs
@@ -2,6 +2,11 @@ namespace Moongate.Server.Data.Internal.Scripting;
 
 public static partial class MobileCustomParamKeys
 {
+    public static class Combat
+    {
+        public const string DamageTypes = "mobile_damage_types";
+    }
+
     public static class Loot
     {
         public const string LootTables = "mobile_loot_tables";
