d42d92425ce3e88a8574d798264dff0386a7dc69
Tom <squid@stormwind.it>
2026-07-24 12:29:45 +0200

    feat(scripting): warn when an off-loop Lua callback drops its result

--- tests/Moongate.Tests/Server/Scripting/LoopAffineInvokeMarshallerTests.cs
+++ tests/Moongate.Tests/Server/Scripting/LoopAffineInvokeMarshallerTests.cs
@@ -1,6 +1,9 @@
 using Moongate.Core.Interfaces;
 using Moongate.Scripting;
 using MoonSharp.Interpreter;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 using SquidStd.Services.Core.Services;
 
 namespace Moongate.Tests.Server.Scripting;
@@ -39,6 +42,32 @@ public class LoopAffineInvokeMarshallerTests
         dispatcher.DrainPending();
 
         Assert.True(ran);
+    }
+
+    [Fact]
+    public void OffLoopThread_DroppingNonNilResult_Warns()
+    {
+        var sink = new ListSink();
+        var dispatcher = new MainThreadDispatcherService();
+        var marshaller = new LoopAffineInvokeMarshaller(
+            new StubLoopThread(false),
+            dispatcher,
+            new LoggerConfiguration().MinimumLevel.Verbose().WriteTo.Sink(sink).CreateLogger()
+        );
+
+        marshaller.Invoke(() => DynValue.NewNumber(42));
+
+        dispatcher.DrainPending();
+
+        Assert.Contains(sink.Events, e => e.Level == LogEventLevel.Warning);
+    }
+
+    private sealed class ListSink : ILogEventSink
+    {
+        public List<LogEvent> Events { get; } = [];
+
+        public void Emit(LogEvent logEvent)
+            => Events.Add(logEvent);
     }
 
     [Fact]
