0ffc583a78f776ca86ef0eec4bee9921de2b5a55
tgiachi <squid@stormwind.it>
2026-09-11 17:42:12 +0200

    feat(server): add console host and Docker setup

---
+++ src/Moongate.Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleAppFramework;
+using Serilog;
+
+await ConsoleApp.RunAsync(
+    args,
+    () =>
+    {
+
+
+        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+        Log.Logger.Information("TERM is {Term}", Environment.GetEnvironmentVariable("TERM") );
+        Log.Logger.Information("Starting up");
+        Log.Logger.Error("Error");
+    }
+);
